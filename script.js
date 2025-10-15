dd0f6934b64fb6fef5abe760f5346522b691ea89
Ebbe <59743608+marco-borsato@users.noreply.github.com>
2025-10-15 14:26:16 +0200

    changed to single page layout

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
+// Display current year automatically
 document.getElementById("year").textContent = new Date().getFullYear();
-
-document.getElementById("learnMoreBtn").addEventListener("click", () => {
-    document.getElementById("about").scrollIntoView({ behavior: "smooth" });
-});
-
-document.getElementById("contactForm").addEventListener("submit", (e) => {
-    e.preventDefault();
-    alert("Thanks for reaching out! We’ll get back to you soon.");
-});
